17c29c773102c1b6bd743471f5b5674bd5009c56
akarshacc <akarsh.vidyarthi@accenture.com>
2026-07-02 12:51:08 +0530

    Document SQL injection vulnerability in xsss.java
    Added example of SQL injection vulnerability in comments.

--- xsss.java
+++ xsss.java
@@ -9,3 +9,10 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response)
     out.println("User comment: " + comment); 
     out.println("</body></html>");
 }
+//hi this akarsh
+// Example 1: SQL Injection Vulnerability
+String userInput = request.getParameter("username");
+String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+// Dangerous: directly concatenating user input into SQL
+Statement stmt = connection.createStatement();
+ResultSet rs = stmt.executeQuery(query);
